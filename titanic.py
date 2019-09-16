bec0e146ee2b834086a83826846273601a2e5a38
rboyes <rboyes@gmail.com>
2019-09-16 22:03:41 +0100

    Stab

--- titanic.py
+++ titanic.py
@@ -1,13 +1,34 @@
 import joblib
 from datetime import datetime
-import pandas as pd
+import pandas
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class Preprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, df: pandas.DataFrame):
+        dfc = df.copy(deep=True)
+
+        title_lookup = {"Mr.": "Mr", "Mrs.": "Mrs", "Miss.": "Miss", "Master.": "Master", "Dr.": "Dr"}
+
+        for key in title_lookup.keys():
+            title = title_lookup[key]
+
+            dfc.loc[dfc["Name"].str.contains(key, regex=False), "Title"] = title
+
+        return dfc
+
 
 train_path = "train.csv"
 test_path = "test.csv"
@@ -17,8 +38,8 @@ params_path = "params_%Y%m%d_%H%M%S.pkl"
 
 timestamp = datetime.now()
 
-train_df = pd.read_csv(train_path, low_memory=False)
-test_df = pd.read_csv(test_path, low_memory=False)
+train_df = pandas.read_csv(train_path, low_memory=False)
+test_df = pandas.read_csv(test_path, low_memory=False)
 
 target_column = 'Survived'
 
@@ -31,24 +52,19 @@ categorical_transformer = make_pipeline(SimpleImputer(strategy='constant', fill_
                                         OneHotEncoder(handle_unknown='ignore'))
 
 numeric_features = ['Age', 'Fare']
-numeric_transformer = make_pipeline(SimpleImputer(strategy='mean'), StandardScaler())
+numeric_transformer = make_pipeline(SimpleImputer(strategy='median'))
 
-name_transformer = CountVectorizer(max_features=128)
-
-ticket_transformer = CountVectorizer(max_features=12)
+preprocessor = Preprocessor()
 
 column_transformer = ColumnTransformer(transformers=[('cat', categorical_transformer, categorical_features),
-                                                     ('num', numeric_transformer, numeric_features),
-                                                     ('name', name_transformer, 'Name'),
-                                                     ('ticket', ticket_transformer, 'Ticket')])
+                                                     ('num', numeric_transformer, numeric_features)])
 
 classifier = RandomForestClassifier(n_estimators=100)
 
-pipeline = make_pipeline(column_transformer, classifier)
+pipeline = make_pipeline(preprocessor, column_transformer, classifier)
 
 grid_params = {
-    'columntransformer__name__max_features': [10, 20, 50, 100],
-    'randomforestclassifier__max_depth': [4, 8, 16],
+    'randomforestclassifier__max_depth': [4, 8, 10, 12, 16],
     'randomforestclassifier__n_estimators': [50, 100, 200, 400]
 }
 
@@ -65,7 +81,6 @@ joblib.dump(best_params, timestamp.strftime(params_path), compress=1)
 test_df[target_column] = best_pipeline.predict(test_df)
 test_df.to_csv(timestamp.strftime(predict_path), index=False)
 
-#import matplotlib
-#matplotlib.use('TkAgg')
-#import matplotlib.pyplot as plt
-
+# import matplotlib
+# matplotlib.use('TkAgg')
+# import matplotlib.pyplot as plt
